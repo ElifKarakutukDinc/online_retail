4be4a5b79c0d239ea7206f03e274ac7c2f662f4f
ElifKarakutukDinc <elifkaraktk@gmail.com>
2020-06-09 15:46:03 +0200

    Creating new table-retail

---
+++ creating_table_for_online_retail.sql
@@ -0,0 +1,13 @@
+--I created this table for loading the data of online_retail's DB. 
+
+create table retail ( 
+InvoiceNo	VARCHAR(150),
+StockCode	VARCHAR(150),
+Description	VARCHAR(150),
+Quantity	INTEGER,
+InvoiceDate	timestamp,
+UnitPrice	FLOAT, 
+CustomerID	INTEGER,
+Country	VARCHAR(150)); 
+
+
